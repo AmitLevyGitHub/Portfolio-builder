acc6ff6b0fcdf4bc73017a17bb068f0d5af83d97
amit levy <amit.p.levy@gmail.com>
2019-01-23 14:23:06 +0200

    few changes

--- controllers/profile.ctl.js
+++ controllers/profile.ctl.js
@@ -4,21 +4,28 @@ const User = require("../models/user"),
 
 module.exports = {
   async showProfile(req, res) {
-    let response = await User.findOne({ id: process.env.ID }, (err, result) => {
+    let id = process.env.ID;
+    let response = await User.findOne({ id: id }, (err, result) => {
       if (err) res.json(errorObj(404, err));
     });
 
-    refactorPhotos(response).then(response => res.json(response));
+    refactorPhotos(response)
+      .then(response => res.json(response))
+      .catch(err => errorObj(404, err));
   }
 };
 
-//function will replace photos id in their url
+//function will replace photos id in photo url
 async function refactorPhotos(response) {
-  for (let i = 0; i < response.__v; i++) {
-    await Photo.findOne({ id: response.photos[i] }, (error, result) => {
-      if (error) res.json(errorObj(404, error));
-      response.photos[i] = result.url;
-    });
+  let refactoredResponse = response;
+  for (let i = 0; i < refactoredResponse.__v; i++) {
+    let result = await Photo.findOne(
+      { id: refactoredResponse.photos[i] },
+      (error, result) => {
+        if (error) res.json(errorObj(404, error));
+      }
+    );
+    refactoredResponse.photos[i] = result.url;
   }
-  return response;
+  return refactoredResponse;
 }
