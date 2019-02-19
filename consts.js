9fa9aee23159df369cdb894b4acca2ff338374ce
amit levy <amit.p.levy@gmail.com>
2019-02-19 12:37:01 +0200

    commit

---
+++ consts.js
@@ -0,0 +1,16 @@
+module.exports = {
+  CLIENT_ID: `7759iu3fwfxudj`,
+  CLIENT_SECRET: `xNb1FQmbhVrj7HqL`,
+  REDIRECT_URI: `http://localhost:3000/authorize`,
+  //`http://localhost:3000/authorize`
+  // `https://portfolio-builder-server-side.herokuapp.com/authorize`,
+  SCOPE: [`r_basicprofile`],
+  STATE: `1q2we3r45t6yu7i8`,
+  UNSPLASH_KEY: `59314f91125a5a9ae1adc2cfc69120348740a942b53339135d03263aa2443197`,
+  UNSPLASH_SECRET: `183498696cde83b848f524b6e973af7989a91c7dd109e259266539640bf244c3`,
+  MLAB_URL: `mongodb://db_usr:db_pass1@ds163164.mlab.com:63164/portfolio-builder`,
+  DB_USER: `db_user`,
+  DB_PASS: `db_pass1`,
+  YOUTUBE_KEY: `AIzaSyBrcAcUqEJwLQ85_5ikMttbTrEXzhltJ5Y`
+};
+//AIzaSyDZt5oN4OipxiKKxG8rVKR7W1V405M4EFs
